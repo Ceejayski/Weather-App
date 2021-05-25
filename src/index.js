652bc98a67e0a817ba92be748237e8f03de35583
Ceejayski <sirchijioke1@gmail.com>
2021-05-25 17:58:26 +0100

    Setup Packages

--- src/index.js
+++ src/index.js
@@ -1,4 +1,40 @@
-import moment from 'moment';
-const today = moment()
+import './style.scss';
+import Store from './Storage/store'
+import Weather from './utils/weather'
+import Geofind from './utils/geocode'
 
-console.log(today.isoWeek())
+const weatherStore = Store.getlocation()
+const weather = new Weather(weatherStore.latt, weatherStore.long)
+const geocode = new Geofind()
+const temp = document.querySelectorAll('temp')
+const container = document.querySelector('.container')
+
+document.addEventListener('DOMContentLoaded', geocode.getWeather(weather));
+
+document.getElementById('location-search').addEventListener('submit', (e) => {
+  const search = document.getElementById('search').value
+  geocode.getLocation(search,weather)
+
+
+  e.preventDefault()
+})
+
+window.changeUnits = () => {
+  const temp = document.querySelectorAll('.temp')
+  const unitsBtn = document.querySelector('#unit-change')
+  for (var i = 0, len = temp.length; i < len; i++){
+    let tempValue = temp[i]
+    if (tempValue.textContent.indexOf('℃') !== -1) {
+      let mainTemp = parseInt(tempValue.textContent.replace('℃', ''))
+      let sol = Math.round(((mainTemp / 5)* 9) + 32)
+      tempValue.textContent = `${sol}°F`
+      unitsBtn.innerHTML = '<span class="text-secondary">℃</span> / <span>°F</span>'
+
+    }else {
+      let mainTemp = parseInt(tempValue.textContent.replace('°F', ''))
+      let sol = Math.round(((mainTemp-32)/9)*5)
+      tempValue.textContent = `${sol}℃`
+      unitsBtn.innerHTML = '<span>℃</span> / <span class="text-secondary">°F</span> '
+    }
+  }
+}
